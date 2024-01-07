451b2296fe98d18ff24b12ce7e7c426462fea60d
ana128@sapo.pt <112607693+codebuil@users.noreply.github.com>
2024-01-07 20:44:57 +0000

    Add files via upload

--- awk.sh
+++ awk.sh
@@ -1,13 +1,16 @@
 awk '{
     if (/>:/) {
-        
         match($0, /</);
         gsub(">", "", $0);
         gsub("<", "", $0);
         print substr($0, RSTART, length($0) - RSTART + 1);
     } else {
-        gsub("<", "", $0);
+        $0=substr($0, 32);
+        if (index($0, "<") > 0) {
+            gsub(/ .*</, "	", $0);
+        }
         gsub(">", "", $0);
-        print substr($0, 32);
+        gsub("<", "", $0);
+        print $0;
     }
 }'
