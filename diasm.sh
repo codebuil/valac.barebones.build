1a5d337193bbc27b1c29cb0ce364887d7e8f0816
ana128@sapo.pt <112607693+codebuil@users.noreply.github.com>
2024-01-07 18:55:46 +0000

    Add files via upload

--- diasm.sh
+++ diasm.sh
@@ -1,2 +1,2 @@
 printf "\ec\e[43;37m\n"
-objdump  -M intel -d -S $1 | awk '/>:/ {print; next} {print substr($0, 32)}'
+objdump  -M intel -d -S $1 | awk '/>:/ { match($0, /</); print substr($0, RSTART+1, length($0) - RSTART + 2); next } {print substr($0, 32)}'
